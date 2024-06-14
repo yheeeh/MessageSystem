ca84770057152545e20a1a22e1e414014026f9dc
yheeeh <ehyhee@gmail.com>
2024-06-14 15:54:33 +0900

    commit

---
+++ messagesystem/src/main/java/com/example/messagesystem/MessagesystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.messagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagesystemApplication.class, args);
+	}
+
+}
---
+++ messagesystem/src/test/java/com/example/messagesystem/MessagesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
